0c65ccc7d5bb27a8eee1230b668b1985e68c75f4
georgekal2798 <38210685+georgekal2798@users.noreply.github.com>
2022-10-07 21:39:40 +0300

    Widows backup script
    - Initial commit

---
+++ backup.bat
@@ -0,0 +1,22 @@
+@echo off
+
+set source=%HOMEPATH%
+set target="D:\Windows Backup"
+set foldersList=Documents Music Pictures Videos
+set timestamp=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2%__%TIME:~0,2%-%TIME:~3,2%
+set timestamp=%timestamp: =0%
+if not exist %target%\Logs mkdir %target%\Logs > nul
+set logFile=%target%\Logs\backup_log_%timestamp%.txt
+
+copy nul %logFile% > nul
+
+echo Backup started at %DATE% %TIME%
+>>%logFile% (
+    echo Backup started at %DATE% %TIME%
+    for %%f in (%foldersList%) do (
+        echo Backing up %%f
+        xcopy %source%\%%f %target%\%%f /e /s /i /d /y
+    )
+    echo Backup finished successfully at %DATE% %TIME%
+)
+echo Backup finished successfully at %DATE% %TIME%
